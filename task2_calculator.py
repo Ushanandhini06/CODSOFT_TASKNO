db94813730a34dc41540eda3f9a57d3471bef595
Ushanandhini06 <ushab.hema@gmail.com>
2026-09-01 20:26:18 +0530

    calculator.py

---
+++ task2_calculator.py
@@ -0,0 +1,21 @@
+print("===== SIMPLE CALCULATOR =====")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        print("Cannot divide by zero.")
+        result = None
+else:
+    print("Invalid operation.")
+    result = None
+if result is not None:
+    print("Result =", result)
